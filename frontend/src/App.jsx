d6fad8d206ac031fadc2f56f0b6e58739356f3d1
tanish <tanish.afre@gmail.com>
2024-09-16 12:39:11 +0100

    Website Running

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState }  from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Routes , Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
+import Login from './components/Login/Login'
+import Payment from './components/Payment/Payment'
+import { StoreContext } from './context/StoreContext'
 
-const App = () => {
+function App() {
+  // const [showLogin , setShowLogin] = useState();
+  const {showLogin} = useContext(StoreContext)
+  useEffect(()=>{
+  },[])
   return (
-    <div>App</div>
+    <>
+    {showLogin?<Login/>:<></>}
+    <div className='app'>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='cart' element={<Cart/>}/>
+      <Route path='order' element={<PlaceOrder/>}/>
+      <Route path='/payment' element={<Payment/>}/>
+    </Routes>
+    </div>
+    <Footer/>
+    </>
   )
 }
 
